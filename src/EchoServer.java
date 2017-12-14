045f58daa66fcaae5488ec6a4d43de57d01e2926
crazysuede <32553130+crazysuede@users.noreply.github.com>
2017-12-14 13:44:38 -0500

    Added Threads. Initial test

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,19 +1,68 @@
 import java.io.*;
 import java.net.*;
 
-public class EchoServer {
+public class EchoServerV2
+{
 
-	public static void main(String[] args) {
+	
+	public static void main (String[] args) 
+	{
+		ServerSocket sock = null;//create sock available for entire method
+		try
+		{
+			System.out.println("main started");
+
+			int id = 1;
+			boolean running = true;
+			sock = new ServerSocket(7000);
+			while (running == true)
+			{
+
+				new Thread(new Connection(id, sock.accept())).start();//create a new thread with connection
+				id++;//
+			}
+		}
+		catch (IOException ioe)
+		{
+			System.err.println(ioe);//output errors
+		}
+		finally
+		{
+			try
+			{
+				sock.close();//Once loop ends, close the ServerSocket
+			}
+			catch (IOException ioe)
+			{
+				System.err.println(ioe);//output errors
+			}
+		}
+
+	}
+
+}
+
+class Connection extends Thread
+{
+	int port;
+	int id;
+	Socket client;
+	public Connection(int idIn, Socket socketIn)
+	{
+		client = socketIn;//get Socket from main thread and use it for future connections
+		id = idIn;
+		System.out.println("Connection Class Made...");
+		newConn();
+	}
+	public void newConn()//method that threads will call
+	{
 		try 
 		{
 			String message = "";// Message received
-			ServerSocket sock = new ServerSocket(7000);// port to listen on
-			// accept connection
-			System.out.println("Connection successful");// if successful
+			System.out.println("Connection successful! ID: " + id);// if successful
 			
-			while (!sock.isClosed()) 
+			while (!client.isClosed()) 
 			{
-				Socket client = sock.accept();
 				byte [] buffer = new byte[512];
 				InputStream input = client.getInputStream();
 				while ((input.read(buffer)) != -1) 
@@ -27,7 +76,7 @@ public class EchoServer {
 
 
 			}
-			sock.close();
+			client.close();
 		} catch (IOException ioe) {
 			System.err.println(ioe);
 		}
@@ -35,7 +84,12 @@ public class EchoServer {
 		{
 			System.out.println("Server has finished");
 		}
+	}
 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
